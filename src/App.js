9cd426d21fdfb38c6c99fa339fc85a9d2fb388e7
KingWahley <kingwahley@gmail.com>
2026-01-01 12:46:46 +0100

    footer stays to bottom

--- src/App.js
+++ src/App.js
@@ -7,25 +7,39 @@ import AnimatedRoutes from './components/AnimatedRoutes/AnimatedRoutes';
 
 
 function App() {
-
-
   return (
     <BrowserRouter>
       <HelmetProvider>
         <Helmet>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,900&family=Roboto:wght@100;300;400;500;700;900&family=Sora:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,900&family=Roboto:wght@100;300;400;500;700;900&family=Sora:wght@100;300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
         </Helmet>
-        <GlobalStyles/>
-        <div className="App">
-          <Nav/>
-          <AnimatedRoutes/>
-          <Footer/>
+
+        <GlobalStyles />
+
+        <div
+          style={{
+            minHeight: "100vh",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Nav />
+
+          <div style={{ flex: 1 }}>
+            <AnimatedRoutes />
+          </div>
+
+          <Footer />
         </div>
       </HelmetProvider>
     </BrowserRouter>
   );
 }
+
 
 export default App;
